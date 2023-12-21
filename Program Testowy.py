a8e176b63481ad47f035348ebd8bf1eaae65338b
Iamapr0grammer <127696002+Iamapr0grammer@users.noreply.github.com>
2023-12-21 19:26:35 +0100

    Add files via upload

---
+++ Program Testowy.py
@@ -0,0 +1,20 @@
+import random
+
+# pierwszy program: Generator haseł
+
+losoweCyfryIZnaki = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+licznik = -1
+
+for i in losoweCyfryIZnaki:
+    licznik += 1
+
+# print(licznik)
+
+dlogoscHasla = int(input("Podaj dlugosc hasla: "))
+haslo = ""
+
+for i in range(dlogoscHasla):
+    haslo += losoweCyfryIZnaki[random.randint(0, licznik)]
+
+print(haslo)
